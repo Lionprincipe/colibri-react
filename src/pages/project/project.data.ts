6b0bd68ebc1d7a9e095c78893d7b77cdb4e9def1
Lionel <lionprincipe@hotmail.com>
2021-03-08 12:49:16 +0100

    start pages and remove empty HOC folder

---
+++ src/pages/project/project.data.ts
@@ -0,0 +1,109 @@
+export const data = [
+  {
+    text: 'COLIBRI IST DIE VOGELPERSPEKTIVE EINER SPANNENDEN LAGE',
+    type: 'h1',
+  },
+  {
+    url: '',
+    title: 'film ansehen',
+    type: 'video',
+  },
+  {
+    url: '',
+    type: 'image',
+  },
+  {
+    text:
+      'vom bezaubernsten Vogel der Welt inspiriert: Das Colibri-Lebensgefühl',
+    type: 'h1',
+  },
+  {
+    text: 'Schützplatz 1, 1140 Wien',
+    type: 'h2',
+  },
+
+  {
+    text:
+      'Behende schwingt er sich empor und hält Ausschau nach neuem Lebensraum: der Colibri, die Verkörperung purer Freude. Er verzaubert auf den ersten Blick und zieht mit jedem Flügelschlag seine Umgebung mehr und mehr in den Bann. Dieses fliegende Juwel steht Pate für ein leichtes, unbeschwertes Lebensgefühl im Westen Wiens. Hier am Schützplatz entsteht Freiraum inmitten urbaner Möglichkeiten, eingebettet im lebendigen Innenhof mit viel Platz unter freiem Himmel. Dort spürt man sie: Gemeinschaft unter Nachbarn und schillernde Inspiration, die jeden Tag neu beflügelt.',
+    type: 'p',
+  },
+  {
+    text: 'Baustart Herbst 2019 // Fertigstellung Herbst 2021',
+    type: 'h2',
+  },
+
+  {
+    text: 'mit 62 Eigentumswohnungen',
+    headline: 'MODERNER NEUBAU',
+    type: 'textWithHeadline',
+  },
+  {
+    text: 'in 7 Min. zur Mariahilfer Strasse',
+    headline: 'ATTRAKTIVE LAGE',
+    type: 'textWithHeadline',
+  },
+  {
+    text: 'von 1 bis 5 Zimmer',
+    headline: '30 BIS 135 M²',
+    type: 'textWithHeadline',
+  },
+  {
+    text: 'von 1 bis 5 Zimmer',
+    headline: '30 BIS 135 M²',
+    type: 'textWithHeadline',
+  },
+  {
+    url: '#',
+    type: 'image',
+  },
+  {
+    url: '#',
+    type: 'image',
+  },
+  {
+    text: 'DER LEBENSRAUM: VON NATUR AUS URBAN.',
+    type: 'h1',
+  },
+  {
+    text:
+      'Breitensee im 14. Bezirk ist für viele ein Geheimtipp. Der Bezirksteil von Penzing im Westen Wiens bedeutet urbanes Leben in einem gewachsenen Gründerzeitviertel. Eine ideale Heimat für Menschen, die sich niederlassen möchten, um in angenehmer Nachbarschaft individuellen Freiraum zu gestalten.',
+    type: 'p',
+  },
+  {
+    text: 'zur lage',
+    type: 'btn',
+  },
+  {
+    type: 'h2',
+    text: 'Schützplatz 1, 1140 Wien',
+  },
+  {
+    type: 'image',
+    url: '',
+  },
+  {
+    type: 'h2',
+    text: 'Strassenansicht',
+  },
+  {
+    text: 'Modernität und Behaglichkeit in schöner Symbiose.',
+    type: 'h1',
+  },
+  {
+    text:
+      'COLIBRI schenkt harmonischem Wohngefühl eine ausdrucksstarke Form. Umgeben von Gründerzeitbauten ist der Neubau ein zeitlos-moderner Blickfang, der Hochwertigkeit mit Charme verbindet.',
+    type: 'p',
+  },
+  {
+    type: 'image',
+    url: '',
+  },
+  {
+    type: 'image',
+    url: '',
+  },
+  {
+    type: 'h2',
+    text: 'Lebendiger Innenhof',
+  },
+]
